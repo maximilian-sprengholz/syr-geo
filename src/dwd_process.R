5b9b4662bf5ef26ee2c2ba9678d42dc5f40171fe
Maximilian Sprengholz <maximilian.sprengholz@hu-berlin.de>
2024-07-05 18:01:24 +0200

    Implemented passthru for just calling export; solved projection problem for 5min data

--- src/dwd_process.R
+++ src/dwd_process.R
@@ -17,6 +17,12 @@ library(tibble)
 library(dplyr)
 library(sjlabelled)
 
+# CRAN only
+#install.packages("rdwd")
+library(rdwd)
+#install.packages("dwdradar")
+library(dwdradar)
+
 ### Shapefiles PLZ (info from 31.12.2021, accessed Feb 28, 2023; last updated Feb 2022)
 # https://downloads.suche-postleitzahl.org/v2/public/plz-5stellig.shp.zip
 p_postcode <- vect(paste0(data, "/external/raw/Shapefiles/plz/plz-5stellig.shp"))
@@ -30,7 +36,7 @@ c_postcode <- centroids(p_postcode, inside = TRUE) # centroids
 # 1. reads in a raster file
 # 2. aggregates using passed vector of c(aggfunction, subset)
 # 3. aggregates by passed index (either integer or one of the predefined time values, e.g. "month")
-sragg <- function (file, aggfun, aggby) {
+sragg <- function (file, aggfun, aggby, cores) {
   # read
   r <- rast(file)
   # aggregate (just reads the file if nothing is specified)
@@ -46,19 +52,24 @@ sragg <- function (file, aggfun, aggby) {
     }
     # aggregate 
     if (is.null(aggby)) {
-      app(r, aggfun[[1]], na.rm = TRUE) # per cell
+      # per cell
+      app(r, aggfun[[1]], na.rm = TRUE, cores = cores)
     } else if (length(aggby) == 1) {
-      tapp(r, aggby, aggfun[[1]], na.rm = TRUE) # per cell x time (e.g. month)
+      # per cell x time (e.g. month)
+      tapp(r, aggby, aggfun[[1]], na.rm = TRUE, cores = cores)
     } else {
-      roll(r, as.numeric(aggby[[2]]), aggfun[[1]], aggby[[1]], na.rm = TRUE) # per cell x moving n time units
+      # per cell x moving n time units, e.g. 14 days up to certain date
+      roll(r, as.numeric(aggby[[2]]), aggfun[[1]], aggby[[1]], na.rm = TRUE, cores = cores)
     }
   }
+  # return
+  return(r)
 }
 
 # stacking function: creates a new SpatRaster combining the layers of each aggregated SpatRaster
-srstack <- function(aggfun, files, aggby) {
+srstack <- function(aggfun, files, aggby, cores) {
   # aggregate each SpatRaster
-  stack <- sapply(files, sragg, aggfun, aggby, simplify = FALSE, USE.NAMES = FALSE)
+  stack <- sapply(files, sragg, aggfun, aggby, cores, simplify = FALSE, USE.NAMES = FALSE)
   # stack in one SpatRaster; assign temporary, consecutively numbered names
   # naming is consistent if aggr. units are unique per output unit (e.g. months in a year;
   # aggregating over multiple years would have the second january suffixed with 13, and so on)
@@ -77,11 +88,13 @@ srstack <- function(aggfun, files, aggby) {
 #   - [ Saves data extracted from aggr. SpatRaster for list of output geoms (e.g. postcode centr.) ]
 srproc <- function(
     df, # dataframe of units to input by (e.g. years) and corresponding files
-    aggfuns = NULL, # list of aggregation functions and raster subset the functions are applied to
+    aggfuns = list(NULL), # list of aggregation functions and raster subset the functions are applied to
     aggby = NULL, # passed on to terra::tap (one of the named time units, e.g. "month", or index)
+    cdf = TRUE, # write cdf?
     extract = list(), # named list of spatVectors for which to extract values (e.g. postcode centr.)
     path = NULL, # out dir
-    stub = NULL # stub prepended to name of variables / output files
+    stub = NULL, # stub prepended to name of variables / output files
+    cores = NULL
     ) {
   # two columns: input unit and corresponding files
   stopifnot(length(df) == 2)
@@ -100,26 +113,29 @@ srproc <- function(
     # loop over aggegration functions, which form the sds subsets and can be accessed by name
     # creates list of aggregated SpatRasters, then stack to SpatRasterDataset
     # name stack by aggregation function names
-    stack <- sapply(aggfuns, srstack, files = infiles, aggby = aggby)
+    # if no aggfun is specified, the default is just a passthru with no agg
+    stack <- sapply(aggfuns, srstack, files = infiles, aggby = aggby, cores = cores)
     for (n in names(stack)) {
-      names(stack[[n]]) <- gsub("^.+_", n, names(stack[[n]]))
+      if (is.null(aggfuns[[1]])) names(stack[[n]]) <- gsub("^.+_", n, names(stack[[n]]))
       if (!is.null(stub)) names(stack[[n]]) <- paste(stub, names(stack[[n]]), sep = "_")
     }
     stack <- sds(stack)
     # save
-    message("OK")
     if (!is.null(path)) {
-      # save for grid
-      outfile <- paste0(
-        path, "/", ifelse(is.null(stub), paste(unit), paste(stub, unit, sep = "_")), "_grid.nc"
-        )
-      writeCDF(stack, file = outfile, overwrite = TRUE)
-      log <- rbind(
-        log, 
-        data.frame(
-          unit, outfile, geo = "grid", paste(varnames(stack), sep = ", "), paste(aggby, collapse = "")
+      if (cdf == TRUE) {
+        # save for grid
+        outfile <- paste0(
+          path, "/", ifelse(is.null(stub), paste(unit), paste(stub, unit, sep = "_")), "_grid.nc"
+          )
+        writeCDF(stack, file = outfile, overwrite = TRUE)
+        log <- rbind(
+          log, 
+          data.frame(
+            unit, outfile, geo = "grid", paste(varnames(stack), sep = ", "), paste(aggby, collapse = "")
           )
         )
+        message("OK")
+      }
       # extract and save value for particular geometries as Stata file
       for (geo in names(extract)) {
         outfile <- paste0(
@@ -130,6 +146,7 @@ srproc <- function(
         values <- as.data.frame(
           extract(stack, project(extract[[geo]], crs(stack)), ID = FALSE, bind = TRUE)
         )
+        message("OK")
         colnames(values) <- gsub("\\.\\d+", "", colnames(values)) # the geo column is duplicated 
         values <- values[, !duplicated(colnames(values))]
         sjlabelled::write_stata(values, outfile, version = 14)
@@ -150,6 +167,21 @@ srproc <- function(
     return(stack) 
   }
 }
+
+# define output units and corresponding files
+yf <- data.frame(day = 1:365)
+yf$input <- paste0(
+  paste0(data, "/external/raw/DWD/pr_sum_5min/2022/"),
+  list.files(paste0(data, "/external/raw/DWD/pr_sum_5min/2022"))
+  )
+# call processing function
+log <- srproc(
+  yf,
+  path = paste0(data, "/external/processed/DWD/pr/survey"),
+  stub = "pr_survey",
+  cdf = FALSE,
+  extract = list(postcode = c_postcode)
+  )
 
 
 ### precipitation ##################################################################################
@@ -175,7 +207,8 @@ log <- srproc(
   aggby = "month",
   path = paste0(data, "/external/processed/DWD/pr/historical"),
   stub = "pr_hist",
-  extract = list(postcode = c_postcode)
+  extract = list(postcode = c_postcode),
+  cores = 3
   )
 # write out cleaned log with added info
 colnames(log) <- c("fileref", colnames(log)[2:length(colnames(log))])
@@ -234,40 +267,22 @@ write.table(
   )
 
 
-### survey time (5 minute intervals -> no aggregation)
+### survey time (5 minute intervals -> no aggregation, just passthru)
 
 # define output units and corresponding files
 yf <- data.frame(day = 1:365)
-yf$input <- data.frame(input = list.files(paste0(data, "/external/raw/DWD/pr_sum_5min/2022")))
-
-# manually extract values and save to Stata
-log <- sapply(
-  yf$input,
-  function(r, extract) {
-    for (geo in names(extract)) {
-      outfile <- paste0(
-        path, "/", 
-        ifelse(is.null(stub), paste(unit, geo, sep = "_"), paste(stub, unit, geo, sep = "_")),
-        ".dta"
-      )
-      values <- as.data.frame(
-        extract(stack, project(extract[[geo]], crs(stack)), ID = FALSE, bind = TRUE)
-      )
-      colnames(values) <- gsub("\\.\\d+", "", colnames(values)) # the geo column is duplicated 
-      values <- values[, !duplicated(colnames(values))]
-      sjlabelled::write_stata(values, outfile, version = 14)
-      log <- rbind(
-        log, 
-        data.frame(
-          unit, outfile, geo, paste(varnames(stack), sep = ", "), paste(aggby, collapse = "")
-        )
-      )
-    }
-  },
+yf$input <- paste0(
+  paste0(data, "/external/raw/DWD/pr_sum_5min/2022/"),
+  list.files(paste0(data, "/external/raw/DWD/pr_sum_5min/2022"))
+  )
+# call processing function
+log <- srproc(
+  yf,
+  path = paste0(data, "/external/processed/DWD/pr/presurvey"),
+  stub = "pr_presurvey",
+  cdf = FALSE,
   extract = list(postcode = c_postcode)
-
-
-
+  )
 
 # write out cleaned log with added info
 colnames(log) <- c("fileref", colnames(log)[2:length(colnames(log))])
@@ -285,6 +300,7 @@ write.table(
   col.names = FALSE,
   append = TRUE
   )
+
 
 
 ### temperature ####################################################################################
@@ -347,17 +363,65 @@ log <- srproc(
 
 ### testing area ###################################################################################
 
-r1 <- rast(yf[1,2])
-names(r1) <- rep(paste0("testnames1_", 1:31), 12)[1:365]
-r2 <- rast(yf[2,2])
-names(r2) <- rep(paste0("testnames2_", 1:31), 12)[1:366]
-t <- list(r1, r2)
-names(t) <- c("lol", "lol")
-t <- rast(t)
-names(t) <- paste0("lol_", 1:length(names(t)))
-tt <- as.data.frame(extract(t, project(c_postcode, crs(t)), ID = FALSE, bind = TRUE))
+crs <- "PROJCS[\"Radolan projection\",
+    GEOGCS[\"Radolan Coordinate System\",
+        DATUM[\"Radolan Kugel\",
+            SPHEROID[\"Erdkugel\",6370040.0,0.0,
+            LENGTHUNIT[\"metre\",1]]],
+        PRIMEM[\"Greenwich\",0,
+            ANGLEUNIT[\"Degree\",0.017453292519943295]]],
+    CONVERSION[\"North_Pole_Stereographic\",
+        METHOD[\"Polar Stereographic (variant A)\",
+            ID[\"EPSG\",9810]],
+        PARAMETER[\"Latitude of natural origin\",90,
+            ANGLEUNIT[\"Degree\",0.017453292519943295],
+            ID[\"EPSG\",8801]],
+        PARAMETER[\"Longitude of natural origin\",10,
+            ANGLEUNIT[\"Degree\",0.017453292519943295],
+            ID[\"EPSG\",8802]],
+        PARAMETER[\"Scale factor at natural origin\",1,
+            SCALEUNIT[\"unity\",0.9330127019],
+            ID[\"EPSG\",8805]],
+        PARAMETER[\"False easting\",0,
+            LENGTHUNIT[\"metre\",1],
+            ID[\"EPSG\",8806]],
+        PARAMETER[\"False northing\",0,
+            LENGTHUNIT[\"metre\",1],
+            ID[\"EPSG\",8807]]],
+        PARAMETER[\"Latitude of standard parallel\",60,
+            ANGLEUNIT[\"Degree\",0.017453292519943295],
+            ID[\"EPSG\",8832]],
+    CS[Cartesian,2],
+        AXIS[\"(E)\",south,
+            ANGLEUNIT[\"degree\",0.017453292519943295],
+            ORDER[1],
+            LENGTHUNIT[\"metre\",1]],
+        AXIS[\"(N)\",south,
+            MERIDIAN[180,
+                ANGLEUNIT[\"degree\",0.017453292519943295]],
+            ORDER[2],
+            LENGTHUNIT[\"metre\",1]]
+    ]"
 
-t <- rast(paste0(data, "/external/processed/DWD/pr/historical_monthly/pr_1995_grid.nc"))
-tt <- extract(t, project(c_postcode, crs(t)), ID = FALSE, bind = TRUE)
-tt <- as.data.frame(tt, check.names = FALSE)
-colnames(tt)
+w <- vect(paste0(data, "/external/raw/DWD/NUTS_RG_20M_2021_3035/NUTS_RG_20M_2021_3035.shp"))
+t <- rast(paste0(data, "/external/raw/DWD/pr_sum_5min/2022/YW_2017.002_20221201.nc"), drivers="NETCDF")
+t <- t[[1]]
+crs(t) <- crs
+t <- project(t, crs(w))
+rad <- vect(paste0(data, "/external/raw/DWD/RADOLAN-GIS/rahmen_radolan_wgs84.shp"))
+r <- rast(paste0(data, "/external/raw/DWD/pr_sum_daily/pr_hyras_1_1995_v5-0_de.nc"))
+r <- r[[1]]
+
+plot(is.na(project(r, crs(w))), alpha = 1)
+plot(w, add = T)
+plot(project(rad, crs(w)), alpha = 0.5, add = TRUE)
+plot(is.na(t), alpha = 0.5, add = TRUE, col = c("#ffffff", "#000000"))
+
+
+
+t <- readDWD(paste0(data, "/external/raw/DWD/test/YW2017.002_202212/YW2017.002_20221201.tar.gz"))
+plotRadar(t$dat, main=".binary RW", extent="rw", layer=1)
+
+
+t <- rast(paste0(data, "/external/raw/DWD/test/RW2017.002_2022_netcdf/2022/RW_2017.002_202201.nc"))
+plot(t[[1]])
